ad9f98818cd72a5b11a698011d09f8f8e4ab25e8
sian-rd <sian.robinsondavies@senadgroup.com>
2025-03-31 09:20:02 +0100

    Create menu_students.js

---
+++ data/menu_students.js
@@ -0,0 +1,65 @@
+const menu = {
+    a1: {
+        category: 'food',
+        type: 'sausages',
+        price: 3 
+    },
+    a2: {
+        category: 'food',
+        type: 'soup',
+        price: 1
+    },
+    a3: {
+        category: 'food',
+        type: 'quiche',
+        price: 3
+    }, 
+    a4: {
+        category: 'food',
+        type: 'pizza',
+        price: 2
+    },
+    a5: {
+        category: 'food',
+        type: 'salad',
+        price: 0.5
+    },
+    a6: {
+        category: 'food',
+        type: 'chips',
+        price: 0.5
+    },
+    a7: {
+        category: 'drink',
+        type: 'lemonade',
+        price: 0.5
+    },
+    a8: {
+        category: 'drink',
+        type: 'tea',
+        price: 0.5
+    },
+    a9: {
+        category: 'drink',
+        type: 'water',
+        price: 0
+    },
+    a10: {
+        category: 'drink',
+        water: 'squash',
+        price: 0.25
+    }
+}   
+    
+
+const person1 = {
+    firstName: 'jim',
+    basket: ['a3', 'a6', 'a5', 'a10'],
+    wallet: 2
+}
+
+const person2 = {
+    firstName: 'rita',
+    basket: ['a2', 'a6', 'a8'],
+    wallet: 10
+}
