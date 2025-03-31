ac06fcc4bc369f65d830aae60f0e05178cbc7a3a
sian-rd <sian.robinsondavies@senadgroup.com>
2025-03-31 09:46:48 +0100

    Update menu data

--- data/menu_students.js
+++ data/menu_students.js
@@ -5,61 +5,66 @@ const menu = {
         price: 3 
     },
     a2: {
-        category: 'food',
-        type: 'soup',
-        price: 1
-    },
-    a3: {
-        category: 'food',
-        type: 'quiche',
-        price: 3
-    }, 
-    a4: {
-        category: 'food',
-        type: 'pizza',
-        price: 2
-    },
-    a5: {
-        category: 'food',
-        type: 'salad',
-        price: 0.5
-    },
-    a6: {
-        category: 'food',
-        type: 'chips',
-        price: 0.5
-    },
-    a7: {
-        category: 'drink',
-        type: 'lemonade',
-        price: 0.5
-    },
-    a8: {
-        category: 'drink',
-        type: 'tea',
-        price: 0.5
-    },
-    a9: {
         category: 'drink',
         type: 'water',
         price: 0
     },
-    a10: {
+    a3: {
+        category: 'food',
+        type: 'soup',
+        price: 1
+    },
+    a4: {
+        category: 'food',
+        type: 'quiche',
+        price: 3
+    }, 
+    a5: {
+        category: 'food',
+        type: 'pizza',
+        price: 2
+    },
+    a6: {
+        category: 'drink',
+        type: 'lemonade',
+        price: 0.5
+    },
+    a7: {
+        category: 'food',
+        type: 'salad',
+        price: 0.5
+    },
+    a8: {
+        category: 'food',
+        type: 'chips',
+        price: 0.5
+    },
+    a9: {
         category: 'drink',
         water: 'squash',
         price: 0.25
+    },   
+    a10: {
+        category: 'drink',
+        type: 'tea',
+        price: 0.5
+    },
+    a11: {
+        category: 'drink',
+        type: 'water',
+        price: 0
     }
 }   
     
 
 const person1 = {
     firstName: 'jim',
-    basket: ['a3', 'a6', 'a5', 'a10'],
-    wallet: 2
+    basket: ['a3', 'a8', 'a4', 'a10'],
+    wallet: 3
 }
 
 const person2 = {
     firstName: 'rita',
-    basket: ['a2', 'a6', 'a8'],
-    wallet: 10
+    basket: ['a1', 'a7', 'a11'],
+    wallet: 5
 }
